17da5a775408290b065d7ebe84040eaa647f19a4
Hu9o73 <hbonnell@live.fr>
2025-09-19 12:10:47 +0200

    fix(dataloader): Fix decoding error on winows

--- src/app/services/dataloader.py
+++ src/app/services/dataloader.py
@@ -1,10 +1,10 @@
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 import kagglehub
+import chardet
 
 class Dataloader:
     @staticmethod
     async def load_data() -> pd.DataFrame:
-        return kagglehub.dataset_load(
-            KaggleDatasetAdapter.PANDAS, "advaithsrao/enron-fraud-email-dataset", "enron_data_fraud_labeled.csv"
-        )
+        file_path = kagglehub.dataset_load("advaithsrao/enron-fraud-email-dataset")
+        return pd.read_csv(f"{file_path}/enron_data_fraud_labeled.csv")
