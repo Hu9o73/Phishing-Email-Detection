3b711731f2e764767afdd55f64352e340c43ea45
Manon Aubry <manon.aubry@edu.devinci.fr>
2025-11-14 10:26:20 +0100

    Changements Readme

--- src/__main__.py
+++ src/__main__.py
@@ -9,11 +9,9 @@ import pandas as pd  # noqa: E402
 
 from app.services.utils import clear_console, menu  # noqa: E402
 
-
 def logging_setup():
     logging.getLogger("kagglehub").setLevel(logging.ERROR)
     warnings.filterwarnings("ignore", category=pd.errors.DtypeWarning)
-
 
 async def main():
     clear_console()
